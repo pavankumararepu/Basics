a32e89e47842412f74fc528968f07052db3c0190
Pavankumar Arepu <iosdeveloper.ipa@gmail.com>
2016-04-09 08:05:18 +0530

    Initial Commit

---
+++ Basics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Basics
+//
+//  Created by Pavankumar Arepu on 4/9/16.
+//  Copyright © 2016 PPAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Basics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Basics
+//
+//  Created by Pavankumar Arepu on 4/9/16.
+//  Copyright © 2016 PPAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
